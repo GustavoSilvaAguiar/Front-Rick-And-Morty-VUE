956a5792a272d1589ae8b0f59a859c122fde8986
Gustavo Silva Aguiar <gustavo.aguiar@aliare.co>
2022-08-21 11:03:55 -0300

    filtro por status adicionado

--- src/modules/cards/services/cards.service.js
+++ src/modules/cards/services/cards.service.js
@@ -3,9 +3,9 @@
 import { coreAPI } from "@/apis/apiConfig";
 
 class CardsService {
-    async getAllCharacters(page, name) {
+    async getAllCharacters(page, name, status) {
         try {
-            const data = await coreAPI.get(`/character/?page=${page}&name=${name}`);
+            const data = await coreAPI.get(`/character/?page=${page}&name=${name}&status=${status}`);
             console.log('passou aqui', data)
             return data;
         } catch (error) {
